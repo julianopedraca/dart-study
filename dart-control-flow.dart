cd6af546c095d909fe257d7323459f879bf9dde2
julianopedraca <juliano.pedraca@gmail.com>
2023-05-22 09:35:11 -0300

    control-flow

--- dart-control-flow.dart
+++ dart-control-flow.dart
@@ -1,4 +1,3 @@
-
 void main() {
   /* 
   we got 2 types of control flow
@@ -81,5 +80,29 @@ void main() {
   } else {
     sample = 'hey guys!';
     print('this is the sample text: $sample');
+  }
+
+  int gfg = 1;
+  switch (gfg) {
+    case 1:
+      {
+        print("GeeksforGeeks number 1");
+      }
+      break;
+    case 2:
+      {
+        print("GeeksforGeeks number 2");
+      }
+      break;
+    case 3:
+      {
+        print("GeeksforGeeks number 3");
+      }
+      break;
+    default:
+      {
+        print("This is default case");
+      }
+      break;
   }
 }
